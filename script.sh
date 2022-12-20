8f8bbbf895ff6fbc55be01c1cd5c2dc08da81138
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 11:04:14 +0100

    First version (not functionnal)

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while true; do  
+        mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite > test.txt
+        lignes=`wc -l < test.txt | cut -d ' ' -f 1`
+        echo $lignes
+        if [ $lignes -gt 4 ]; then
+                cat test.txt > archive.txt
+                "" > test.txt
+        fi
+done
