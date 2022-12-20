8d66dcc7245a2539d4f0c4457cf5cc14e4578daf
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 11:19:41 +0100

    Added a ">" character in order to append the content of test.txt

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do  
-        mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite > test.txt
+        mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> test.txt
         lignes=`wc -l < test.txt | cut -d ' ' -f 1`
         echo $lignes
         if [ $lignes -gt 4 ]; then
