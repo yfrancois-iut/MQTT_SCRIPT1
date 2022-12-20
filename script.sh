eb40a0d40e16bdd7403f75def3dd7b8757a65ace
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 11:37:13 +0100

    -Working script > Every 10 mosquitto_sub input, the content is saved in "archive.txt" and test.txt is rewrited upon. Archive.txt is going to be used for statistics purpose : keeping data to have coherent average/min/max values.
    -The debugging feature printing "lignes" has been removed.

--- script.sh
+++ script.sh
@@ -3,9 +3,8 @@
 while true; do  
         mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> test.txt
         lignes=`wc -l < test.txt | cut -d ' ' -f 1`
-        echo $lignes
-        if [ $lignes -gt 4 ]; then
-                cat test.txt > archive.txt
-                "" > test.txt
+        if [ $lignes -gt 10 ]; then
+                cp test.txt archive.txt
+                > test.txt
         fi
 done
