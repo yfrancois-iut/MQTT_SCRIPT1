3d87568c9897a7c06bf90749780ccb01284870ba
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 11:26:01 +0100

    Deleting the content of test.txt at the begining of the script in order to start over with an empty file.

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+> test.txt
 while true; do  
         mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> test.txt
         lignes=`wc -l < test.txt | cut -d ' ' -f 1`
