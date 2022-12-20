34b64c6c9c7d91aa69a6682584f84a7ea7711eed
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 14:19:46 +0100

    Delete script2.sh

--- script2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat archive.txt | cut -d ':' -f 3 | sed 's/}//' > valeurs.txt
-nbr_total=`wc -l < archive.txt | cut -d ' ' -f 1 `
-while IFS="" read -r p || [ -n "$p" ]
-do
- somme=`somme + $p`
- echo $somme
-done < archive.txt
