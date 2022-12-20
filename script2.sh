26eca656dfae43e9674067688901488f98bb13bb
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 20:39:19 +0100

    This version initialises the variables min and max to the first value of the list, then proceeding onto further comparaisons with the rest of it.

--- script2.sh
+++ script2.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+max=0
+min=0
 cat archive.txt | cut -d ':' -f 3 | sed 's/}//' > valeurs.txt
 nbr_total=`wc -l < archive.txt | cut -d ' ' -f 1 `
 while IFS="" read -r p || [ -n "$p" ]
 do
  let somme=$somme+$p
  echo $p
-done < valeurs.txt
-echo $somme
+ if [ $max == 0 ];then
+  max=$p
+ fi
+ if [ $min == 0 ];then
+  min=$p
+ fi
+ if [ $p -gt $max ];then
+  max=$p
+ fi
+ if [ $p -lt $min ];then
+  min=$p
+ fi
+done<valeurs.txt
+echo "La somme de toutes les valeurs est : $somme"
 let moyenne=$somme/$nbr_total
-echo $moyenne
+echo "La moyenne de toutes les valeurs est : $moyenne"
+echo "Le minimum de toutes les valeurs est : $min"
+echo "Le maximums de toutes les valeurs est : $max"
