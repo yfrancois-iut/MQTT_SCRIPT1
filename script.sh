c6114cd6e8bb65bc883afb0912866fb21ddd5478
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 22:41:35 +0100

    Better naming the temporary storage file. Upgrading the number of data stored to 20 lines. Preparing the merge between script.sh and script2.sh.

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-> test.txt
+> temp.txt
 while true; do  
-        mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> test.txt
-        lignes=`wc -l < test.txt | cut -d ' ' -f 1`
-        if [ $lignes -gt 10 ]; then
-                cp test.txt archive.txt
-                > test.txt
-        fi
+ mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> temp.txt
+ lignes=`wc -l < temp.txt | cut -d ' ' -f 1`
+ if [ $lignes -gt 20 ]; then
+  cp temp.txt archive.txt
+  > temp.txt
+ fi
 done
