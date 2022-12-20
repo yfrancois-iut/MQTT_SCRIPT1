81327b1ce8d8212c734ae3735f5d95710233a97a
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 22:30:28 +0100

    Compacting the code.

--- script2.sh
+++ script2.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-max=0
-min=0
+!/bin/bash
+let min=max=0
 cat archive.txt | cut -d ':' -f 3 | sed 's/}//' > valeurs.txt
 nbr_total=`wc -l < archive.txt | cut -d ' ' -f 1 `
 while IFS="" read -r p || [ -n "$p" ]
 do
  let somme=$somme+$p
  echo $p
- if [ $max == 0 ];then
+ if [ $max == 0 ] || [ $min == 0 ];then
   max=$p
- fi
- if [ $min == 0 ];then
   min=$p
  fi
  if [ $p -gt $max ];then
@@ -20,8 +17,8 @@ do
   min=$p
  fi
 done<valeurs.txt
-echo "La somme de toutes les valeurs est : $somme"
 let moyenne=$somme/$nbr_total
+echo "La somme de toutes les valeurs est : $somme"
 echo "La moyenne de toutes les valeurs est : $moyenne"
 echo "Le minimum de toutes les valeurs est : $min"
 echo "Le maximums de toutes les valeurs est : $max"
