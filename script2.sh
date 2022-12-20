a1a02ce4d7fccd3e8666a902486e85f0d98899b4
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-20 19:06:15 +0100

    v2

--- script2.sh
+++ script2.sh
@@ -3,8 +3,8 @@ cat archive.txt | cut -d ':' -f 3 | sed 's/}//' > valeurs.txt
 nbr_total=`wc -l < archive.txt | cut -d ' ' -f 1 `
 while IFS="" read -r p || [ -n "$p" ]
 do
- somme=$somme+$p
- echo $somme
+ let somme=$somme+$p
 done < valeurs.txt
-moyenne=$somme/$nbr_total
+echo $somme
+let moyenne=$somme/$nbr_total
 echo $moyenne
