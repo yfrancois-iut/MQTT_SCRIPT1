14f07cbd01b321142e3196f06d23011b551f31c6
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2022-12-22 16:47:41 +0100

    Saving the date correlated to each of the new entries registered.

--- script.sh
+++ script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 > temp.txt
+> date.txt
 while true; do  
  mosquitto_sub -C 1 -h iot.iut-blagnac.fr -u student -P student -t iut/bate/etage1/+/luminosite >> temp.txt
  lignes=`wc -l < temp.txt | cut -d ' ' -f 1`
+ date +%x-%X >> date.txt
  if [ $lignes -gt 20 ]; then
   cp temp.txt archive.txt
+  cp date.txt datarchive.txt
   > temp.txt
+  > date.txt
  fi
 done
