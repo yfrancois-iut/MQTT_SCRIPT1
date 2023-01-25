2b47a0f4806df96969ff482315d596ca9267921f
yfrancois-iut <121094049+yfrancois-iut@users.noreply.github.com>
2023-01-26 00:54:47 +0100

    Historical min and max

--- script2.0/script.sh
+++ script2.0/script.sh
@@ -35,6 +35,7 @@ fonction_tableau2_site() {
  m2=$7
  min2=$8
  max2=$9
+
 #Naming the file destination for sed
  fichier='./capteurs.html'
 #Naming the first flag that will help perl orientate itself in the HMTL file and allow for dynamic table editing.
@@ -42,9 +43,9 @@ fonction_tableau2_site() {
 #Naming the second flag that will help perl orientate itself in the HMTL file and allow for dynamic table editing.
  balisefin='<!--Tab2fin-->'
 #ligne1-3 are the variables that store the table rows with the actualized variables passed as arguments and stored in the previously mentionned local variables.
- ligne1=$(echo '<th>E101</th> <td class ="gauche">'$m1' lux </td> <td class="droite">'$min1' lux </td> <td class="droite">'$max1' lux </td>')
- ligne2=$(echo '<th>E102</th> <td class ="gauche">'$m2' lux </td> <td class="droite">'$min2' lux </td> <td class="droite">'$max2' lux </td>')
- ligne3=$(echo '<th>Global</th> <td class ="gauche">'$m' lux </td> <td class="droite">'$min' lux </td> <td class="droite">'$max' lux </td>')
+ ligne1=$(echo '<th>E101</th> <td class ="gauche">'$m1' lux </td> <td class="droite">'$min1' lux </td> <td class="droite">'$max1' lux </td> <td class="droite">'${10}' lux </td> <td class="droite">'${11}' lux </td>')
+ ligne2=$(echo '<th>E102</th> <td class ="gauche">'$m2' lux </td> <td class="droite">'$min2' lux </td> <td class="droite">'$max2' lux </td> <td class="droite">'${12}' lux </td> <td class="droite">'${13}' lux </td>')
+ ligne3=$(echo '<th>Global</th> <td class ="gauche">'$m' lux </td> <td class="droite">'$min' lux </td> <td class="droite">'$max' lux </td> <td class="droite">'${14}' lux </td> <td class="droite">'${15}' lux </td>')
 #perl is an interpretor and programming language that we use here as a stream editor like sed, except it is less tideous to replace a range of characters contained by two strings.
 #-0 allows for slurping the file (the 777 following is a big octal number that allows for storing most files passed as an argument.)
 #-i ,like in sed, redirects the output to the file. -pe is a combination of two parameters.
@@ -58,14 +59,14 @@ fonction_tableau2_site() {
 salle=()
 valeur=()
 date=()
-let j=k=i=somme_moyenne=somme_e102=somme_e101=moyenne_e101=moyenne_e102=somme=maximum=minimum=minimum_e101=minimum_e102=maximum_e101=maximum_e102=0
+let jo=ko=io=j=k=i=somme_moyenne=somme_e102=somme_e101=moyenne_e101=moyenne_e102=somme=maximum=minimum=minimum_e101=minimum_e102=maximum_e101=maximum_e102=0
 #This is an infinite loop that begins with a mosquitto_sub. It only lasts for one message so we can work with the data
 #without being blocked by the mosquitto process. Once we treated the data gathered, it will come back to the subscribe stage/phase.
 #The first if loop is about resetting the webpage at every startup of the script, and everytime the index i reaches a certain desired value.
 while true; do
- if [ "$i" -ge "5" ] || [ "$maximum" -eq "0" ];then
+ if [ "$i" -ge "10" ] || [ "$maximum" -eq "0" ];then
   cat ./capteurs_debut.html > capteurs.html
-  curl -u "4183242_yfrancois:Tu76./gh" -T ./capteurs.html ftp://yfrancois.atwebpages.com/SAE15/capteurs.html
+  curl -s -u "4183242_yfrancois:Tu76./gh" -T ./capteurs.html ftp://yfrancois.atwebpages.com/SAE15/capteurs.html
   salle=()
   valeur=()
   date=()
@@ -83,9 +84,9 @@ while true; do
  ajout_date=`date +%x-%X`
  date+=($ajout_date)
 #This down below is a debugging feature that prints the three arrays to make sure that the previous commands do what they are supposed to do.
- echo ${salle[@]}
- echo ${valeur[@]}
- echo ${date[@]}
+# echo ${salle[@]}
+# echo ${valeur[@]}
+# echo ${date[@]}
 #Loop that iterates through every element of the valeur[] array.
  for champ in ${valeur[@]}; do
 #Initalizing maximum and minimum
@@ -101,12 +102,25 @@ while true; do
    minimum=$champ
   fi
  done
+ if [ $io -eq 0 ];then
+  min_historique=$maximum
+  max_historique=$minimum
+ fi
+ if [ $ajout_valeur -gt $max_historique ];then
+  max_historique=$ajout_valeur
+ fi
+ if [ $ajout_valeur -lt $min_historique ];then
+  min_historique=$ajout_valeur
+ fi
+ echo "min histo" $min_historique
+ echo "max histo" $max_historique
 #Initalizing and index that is used to get the current last value of the valeur[] array. ${#valeur[@]} gives out the total number of values in the array.
  if [ ${#valeur[@]} -gt 1 ];then
   let i=i+1
  else
   i=0
  fi
+ let io=io+1
 #All 'echos" are meant for debugging and visualizing the process of the script and the values outputed.
 #Calculus of moyenne (=average) and somme_moyenne(=the sum of every value in the array) in order to calculate the average.
  let somme_moyenne=$somme_moyenne+${valeur[i]}
@@ -128,9 +142,21 @@ while true; do
   if [ $valeur_e101 -lt $minimum_e101 ];then
    minimum_e101=$valeur_e101
   fi
+  if [ $jo -eq 0 ];then
+   min_historique101=$maximum
+   max_historique101=$minimum
+  fi
+  if [ $valeur_e101 -gt $max_historique101 ];then
+   max_historique101=$valeur_e101
+  fi
+  if [ $valeur_e101 -lt $min_historique101 ];then
+   min_historique101=$valeur_e101
+  fi
+  echo "min histo 101" $min_historique101
+  echo "max histo 101" $max_historique101
+  let jo=jo+1
   let j=j+1
   let somme_e101=$somme_e101+$valeur_e101
-  echo "compteur :" $j "somme : " $somme_e101
   moyenne_e101=$(echo "scale=2;$somme_e101/$j" | bc)
  fi
  if [[ "${salle[i]}" == "E102" ]]; then
@@ -145,17 +171,30 @@ while true; do
   if [ $valeur_e102 -lt $minimum_e102 ];then
    minimum_e102=$valeur_e102
   fi
+  if [ $ko -eq 0 ];then
+   min_historique102=$maximum_e102
+   max_historique102=$minimum_e102
+  fi
+  if [ $valeur_e102 -gt $max_historique102 ];then
+   max_historique102=$valeur_e102
+  fi
+  if [ $valeur_e102 -lt $min_historique102 ];then
+   min_historique102=$valeur_e102
+  fi
+  echo "min histo e102" $min_historique102
+  echo "max histo e102" $max_historique102
+  let ko=ko+1
   let k=k+1
   let somme_e102=$somme_e102+$valeur_e102
   moyenne_e102=$(echo "scale=2;$somme_e102/$k" | bc)
  fi
 #Debugging echos
- echo 'E102' $moyenne_e102 $minimum_e102 $maximum_e102
- echo 'E101' $moyenne_e101 $minimum_e101 $maximum_e101
- echo 'global' $moyenne $minimum $maximum
+# echo 'E102' $moyenne_e102 $minimum_e102 $maximum_e102
+# echo 'E101' $moyenne_e101 $minimum_e101 $maximum_e101
+# echo 'global' $moyenne $minimum $maximum
 #Calling a function that takes in 3 parameters/arguments in order to integrate them to an HTML table
  fonction_tableau1_site ${valeur[i]} ${salle[i]} ${date[i]}
- fonction_tableau2_site $moyenne $minimum $maximum $moyenne_e101 $minimum_e101 $maximum_e101 $moyenne_e102 $minimum_e102 $maximum_e102
+ fonction_tableau2_site $moyenne $minimum $maximum $moyenne_e101 $minimum_e101 $maximum_e101 $moyenne_e102 $minimum_e102 $maximum_e102 $min_historique101 $max_historique101 $min_historique102 $max_historique102 $min_historique $max_historique
 #The curl function is used to upload the modified HTML file *capteurs.html* via FTP towards the website
- curl -u "4183242_yfrancois:Tu76./gh" -T ./capteurs.html ftp://yfrancois.atwebpages.com/SAE15/capteurs.html
+ curl -s -u "4183242_yfrancois:Tu76./gh" -T ./capteurs.html ftp://yfrancois.atwebpages.com/SAE15/capteurs.html
 done
